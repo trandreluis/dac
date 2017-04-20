b91a64db8baa2823b49a32e1b715b0f717a1a46d
Andre Luis <tr.andreluis@gmail.com>
2017-04-19 21:53:32 -0300

    Teste nas mensagens

--- 2017.1-crud-jsf/src/main/java/br/edu/ifpb/mt/daca/util/Messages.java
+++ 2017.1-crud-jsf/src/main/java/br/edu/ifpb/mt/daca/util/Messages.java
@@ -6,9 +6,9 @@ import javax.faces.context.Flash;
 
 public class Messages {
 	public static void addFlashMessage(String message) {
-        Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
-        flash.setKeepMessages(true);
-        flash.setRedirect(true);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
-}
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flash.setKeepMessages(true);
+		flash.setRedirect(true);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
+	}
 }
