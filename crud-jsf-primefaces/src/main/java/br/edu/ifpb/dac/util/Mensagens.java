b4f14ddd3133fe18b93baa9510dd494a6447a865
Andre Luis <tr.andreluis@gmail.com>
2017-04-27 03:05:18 -0300

    Implementacao das mensagens de aviso

---
+++ crud-jsf-primefaces/src/main/java/br/edu/ifpb/dac/util/Mensagens.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.dac.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+
+public class Mensagens {
+	public static void MensagemSucesso(String mensagem) {
+
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flash.setKeepMessages(true);
+		flash.setRedirect(true);
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		FacesMessage facesMensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", mensagem);
+        context.addMessage(null,  facesMensagem);
+	
+	}
+}
