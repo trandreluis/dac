33a9a7a92375f2e789439f59f0f3352f06036ab4
Andre Luis <tr.andreluis@gmail.com>
2017-04-27 03:14:28 -0300

    Modificacao das mensagens de alerta e adicao de comentarios de @author

--- crud-jsf-primefaces/src/main/java/br/edu/ifpb/dac/beans/CadastroTarefaBean.java
+++ crud-jsf-primefaces/src/main/java/br/edu/ifpb/dac/beans/CadastroTarefaBean.java
@@ -5,34 +5,44 @@ import javax.faces.bean.ViewScoped;
 
 import br.edu.ifpb.dac.dao.TarefaDAO;
 import br.edu.ifpb.dac.entidades.Tarefa;
+import br.edu.ifpb.dac.util.Mensagens;
 
+/**
+ * 
+ * email: tr.andreluis@gmail.com
+ * 
+ * @author <a href="https://github.com/trandreluis">André Luís</a>
+ *
+ */
 @ManagedBean
 @ViewScoped
 public class CadastroTarefaBean {
-	
+
 	private Tarefa tarefa;
-	
+
 	private TarefaDAO daoTarefa = new TarefaDAO();
 
 	public void preRenderizacao() {
-		if(this.tarefa == null) {
+		if (this.tarefa == null) {
 			this.tarefa = new Tarefa();
 		}
 	}
-	
+
 	public String cadastrar() {
-		if(this.tarefa.getId() == null) {
-			daoTarefa.salvar(this.tarefa);			
+		if (this.tarefa.getId() == null) {
+			daoTarefa.salvar(this.tarefa);
+			Mensagens.MensagemSucesso("A Tarefa foi cadastrada");
 		} else {
 			daoTarefa.atualizar(this.tarefa);
+			Mensagens.MensagemSucesso("A Tarefa foi atualizada");
 		}
 		return "Tarefas?faces-redirect=true";
 	}
-	
+
 	public String editar() {
 		return null;
 	}
-	
+
 	public Tarefa getTarefa() {
 		return tarefa;
 	}
@@ -40,5 +50,5 @@ public class CadastroTarefaBean {
 	public void setTarefa(Tarefa tarefa) {
 		this.tarefa = tarefa;
 	}
-	
+
 }
