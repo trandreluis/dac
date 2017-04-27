04a8c9d50d26f83c88beea730f33b4289d07a427
Andre Luis <tr.andreluis@gmail.com>
2017-04-27 03:16:13 -0300

    Implmentacao do metodo limpar e utilizacao do bindig com p:inputtext, além da adicao de comentarios

--- crud-jsf-primefaces/src/main/java/br/edu/ifpb/dac/beans/TarefasBean.java
+++ crud-jsf-primefaces/src/main/java/br/edu/ifpb/dac/beans/TarefasBean.java
@@ -6,8 +6,18 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import org.primefaces.component.inputtext.InputText;
+
 import br.edu.ifpb.dac.dao.TarefaDAO;
 import br.edu.ifpb.dac.entidades.Tarefa;
+
+/**
+ * 
+ * email: tr.andreluis@gmail.com
+ * 
+ * @author <a href="https://github.com/trandreluis">André Luís</a>
+ *
+ */
 
 @ManagedBean
 @RequestScoped
@@ -16,6 +26,8 @@ public class TarefasBean {
 	private ArrayList<Tarefa> tarefas;
 	
 	private String tituloBuscado;
+	
+	private InputText inputTituloTarefa;
 	
 	private TarefaDAO daoTarefas = new TarefaDAO();
 	
@@ -26,6 +38,11 @@ public class TarefasBean {
 
 	public void buscar() {		
 		tarefas = daoTarefas.buscarPorTitulo(tituloBuscado);
+	}
+	
+	public void limpar() {
+		this.tituloBuscado = "";
+		buscar();
 	}
 	
 	public ArrayList<Tarefa> getTarefas() {
@@ -43,6 +60,14 @@ public class TarefasBean {
 
 	public void setTituloBuscado(String tituloBuscado) {
 		this.tituloBuscado = tituloBuscado;
+	}
+
+	public InputText getInputTituloTarefa() {
+		return inputTituloTarefa;
+	}
+
+	public void setInputTituloTarefa(InputText tituloTarefa) {
+		this.inputTituloTarefa = tituloTarefa;
 	}
 	
 }
